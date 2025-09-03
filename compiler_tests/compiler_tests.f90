02fffa4751729b00203a52c5fc183516d3a87b66
Nekkochan <nekkochan@darkstar.example.net>
2025-09-03 11:39:06 +0700

    Update README.md

--- compiler_tests/compiler_tests.f90
+++ compiler_tests/compiler_tests.f90
@@ -1,3 +1,19 @@
+!
+! compiler_tests.f90 - Compiler tests written in FORTRAN
+! 
+! Copyright (C) 2025 Nguyen Duy Thanh (@Nekkochan0x0007). All right reserved
+!
+! FORTRAN, FORTRAN documentation and compiler references from Intel Corporation. All the references are
+!
+! Copyright (C) 2019 - 2023 Intel Corporation. All right reserved.
+! Please refer to the Intel Corporation for more information.
+!
+! This file is a part of NeRF project
+!
+! Please use the Intel(R) oneAPI Fortran Compiler for building this program.
+!
+! 9/3/2025 at 11:38 AM
+
 program compiler_test
     print *, "Compiler working!"
 end program
